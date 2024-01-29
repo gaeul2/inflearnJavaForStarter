13b45b3c45615a3d36e09141f08cf29e0d2d6924
0540 <0540@coreit.co.kr>
2024-01-29 16:32:38 +0900

    변수 선언과 초기화

---
+++ src/variable/Var6.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+//        System.out.println(a); // 초기값 없이 무언갈 하려고 하면 초기화 안했다고 컴파일 오류남.
+        //**지역변수** 는 꼭 초기화를 해줘야함
+    }
+}
