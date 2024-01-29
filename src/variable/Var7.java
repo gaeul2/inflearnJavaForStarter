3df6951cfb3d312fdd5c249c9f444a0e57c4eb1e
0540 <0540@coreit.co.kr>
2024-01-29 16:44:08 +0900

    변수 타입1

---
+++ src/variable/Var7.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; //정수
+        double b = 10.5; // 실수
+        boolean c = true; //불리언(boolean) true, false 입력가능
+        char d = 'A'; //문자하나를 다룰때 사용 '' 작은따옴표를 사용하여 감싸야함.
+        String e = "Hello Java"; //문자열, 문자열을 다루기위한 특별한 타입 , "" 큰따옴표를 사용해야함
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
